5b7df9503c02d45c0b33d55c03c44edd19aa7431
Caio Encarnação <cencarnacaoq@gmail.com>
2026-06-15 10:31:39 -0300

    add skeleton for logic

--- frontend/app/page.jsx
+++ frontend/app/page.jsx
@@ -1,16 +1,33 @@
+"use client";
+
 import { Card, Eyebrow, Meta, Shell } from "../css/login";
 import { MoveRight } from "lucide-react";
 import { Button, TextBox } from "../css/common";
+import { useState, useContext } from "react";
+import { useUser } from "./providers";
 
 export default function Page() {
+  const [username, setUsername] = useState();
+  const { user, setUser } = useUser();
+  function handleInputName(input) {
+    setUsername(input.target.value);
+  }
+
   return (
     <Shell>
       <Card>
         <Eyebrow>Aplicação de Websocket</Eyebrow>
         <Meta style={{ marginTop: "32px", alignItems: "center" }}>
-          <TextBox placeholder="Entre com um usuário" />
+          <TextBox
+            placeholder="Entre com um usuário"
+            onChange={handleInputName}
+          />
           <Button>
-            <MoveRight />
+            <MoveRight
+              onClick={() => {
+                setUser({ username, websocketId: null });
+              }}
+            />
           </Button>
         </Meta>
       </Card>
