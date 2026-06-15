9c5862481db2bfaf571e4b89674f9aebbbe54b12
Caio Encarnação <cencarnacaoq@gmail.com>
2026-06-15 10:30:55 -0300

    add `UserContext`

--- frontend/app/providers.jsx
+++ frontend/app/providers.jsx
@@ -1,10 +1,23 @@
+"use client";
+
 import GlobalStyle from "../css/global-style";
+import { createContext, useContext, useState } from "react";
+
+const UserContext = createContext(null);
+
+export function useUser() {
+  return useContext(UserContext);
+}
 
 export default function Providers({ children }) {
+  const [user, setUser] = useState(null);
+
   return (
     <>
-      <GlobalStyle />
-      {children}
+      <UserContext.Provider value={{ user, setUser }}>
+        <GlobalStyle />
+        {children}
+      </UserContext.Provider>
     </>
   );
 }
