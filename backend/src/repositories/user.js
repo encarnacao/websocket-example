b924aeffc1f6e73245255313878e8f5cb0c8712b
Caio Encarnação <cencarnacaoq@gmail.com>
2026-06-15 21:26:26 -0300

    fix column name

--- backend/src/repositories/user.js
+++ backend/src/repositories/user.js
@@ -3,12 +3,12 @@ import pgPool from "../database/pg.js";
 export async function getUser(username) {
   const client = await pgPool.connect();
   try {
-    const user = await client.query("SELECT * FROM users WHERE username = $1", [
+    const user = await client.query("SELECT * FROM users WHERE name = $1", [
       username,
     ]);
     if (!user.rows.length) {
       const newUser = await client.query(
-        "INSERT INTO users (username) VALUES ($1) RETURNING *",
+        "INSERT INTO users (name) VALUES ($1) RETURNING *",
         [username],
       );
       return newUser.rows[0];
--- backend/src/routes/user.js
+++ backend/src/routes/user.js
@@ -9,5 +9,5 @@ userRouter.post("/login", async (req, res) => {
     return res.status(400).json({ error: "Username é obrigatório" });
   }
   const user = await getUser(username);
-  res.status(200).json({ userId: user.id, username: user.username });
+  res.status(200).json({ userId: user.id, username: user.name });
 });
